6d50e28e83f33debd67a79bb8fbb0d6503e55107
Víctor González <victor.gonzalez@geomati.co>
2016-08-05 09:33:12 +0200

    Adapt GeoExplorerDBConfigurationProvider to the new interface API.

--- geoexplorer-reader/src/main/java/org/fao/unredd/geoexplorerReader/GeoExplorerDBConfigurationProvider.java
+++ geoexplorer-reader/src/main/java/org/fao/unredd/geoexplorerReader/GeoExplorerDBConfigurationProvider.java
@@ -10,6 +10,7 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import net.sf.json.JSON;
 import net.sf.json.JSONObject;
 import net.sf.json.JSONSerializer;
 
@@ -19,8 +20,9 @@ import org.fao.unredd.portal.ModuleConfigurationProvider;
 import org.fao.unredd.portal.PersistenceException;
 import org.fao.unredd.portal.PortalRequestConfiguration;
 
-public class GeoExplorerDBConfigurationProvider implements
-		ModuleConfigurationProvider {
+public class GeoExplorerDBConfigurationProvider
+		implements
+			ModuleConfigurationProvider {
 
 	public GeoExplorerDBConfigurationProvider() {
 	}
@@ -30,6 +32,23 @@ public class GeoExplorerDBConfigurationProvider implements
 			PortalRequestConfiguration configurationContext,
 			HttpServletRequest request) throws IOException {
 		Map<String, JSONObject> ret = new HashMap<String, JSONObject>();
+		fillConfigMap(configurationContext, request, ret);
+		return ret;
+	}
+
+	@Override
+	public Map<String, JSON> getConfigMap(
+			PortalRequestConfiguration configurationContext,
+			HttpServletRequest request) throws IOException {
+		Map<String, JSON> ret = new HashMap<String, JSON>();
+		fillConfigMap(configurationContext, request, ret);
+		return ret;
+	}
+
+	@SuppressWarnings("unchecked")
+	private <T extends JSON> void fillConfigMap(
+			PortalRequestConfiguration configurationContext,
+			HttpServletRequest request, Map<String, T> ret) {
 		try {
 			String mapIdParameter = request.getParameter("mapId");
 			int mapId;
@@ -37,21 +56,21 @@ public class GeoExplorerDBConfigurationProvider implements
 				try {
 					mapId = Integer.parseInt(mapIdParameter);
 				} catch (NumberFormatException e) {
-					throw new ConfigurationException("mapId must be an integer");
+					throw new ConfigurationException(
+							"mapId must be an integer");
 				}
 			} else {
 				throw new ConfigurationException(
 						"mapId parameter must be configured");
 			}
-			ret.put("geoexplorer-layers", getGeoExplorerLayers(mapId));
+			ret.put("geoexplorer-layers", (T) getGeoExplorerLayers(mapId));
 		} catch (PersistenceException e) {
-			throw new ConfigurationException(
-					"Cannot read geoexplorer database", e);
+			throw new ConfigurationException("Cannot read geoexplorer database",
+					e);
 		}
-		return ret;
 	}
 
-	private JSONObject getGeoExplorerLayers(final int mapId)
+	private JSON getGeoExplorerLayers(final int mapId)
 			throws PersistenceException {
 		String config = DBUtils.processConnection("geoexplorer",
 				new DBUtils.ReturningDBProcessor<String>() {
@@ -60,7 +79,8 @@ public class GeoExplorerDBConfigurationProvider implements
 					public String process(Connection connection)
 							throws SQLException {
 						PreparedStatement statement = connection
-								.prepareStatement("select config from maps where id=?");
+								.prepareStatement(
+										"select config from maps where id=?");
 						statement.setInt(1, mapId);
 						ResultSet rs = statement.executeQuery();
 						if (rs.next()) {
