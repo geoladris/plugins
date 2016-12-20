0ef332f97fd6de7ed0792020a73a240bfd19a633
Víctor González <victor.gonzalez@geomati.co>
2016-12-20 15:14:32 +0100

    Migrate footnote to use ui plugin

--- footnote/src/main/resources/geoladris/footnote/modules/footnote.js
+++ footnote/src/main/resources/geoladris/footnote/modules/footnote.js
@@ -1,20 +1,40 @@
-define(["module", "layout", "i18n", "jquery"], function(module, layout, i18n, $) {
+define([ "message-bus", "module", "layout", "i18n", "jquery", "ui/ui" ], function(bus, module, layout, i18n, $) {
+  var config = module.config();
+  var text = i18n[config.text] || config.text; // Required. Text can be a literal string or an i18n key reference.
+  var link = i18n[config.link] || config.link; // Optional. Link can be a literal string or an i18n key reference.
+  var align = config.align || "center"; // Optional. "left", "center" or "right". Defaults to "center".
 
-	var config = module.config();
-	var text = i18n[config.text] || config.text; // Required. Text can be a literal string or an i18n key reference.
-	var link = i18n[config.link] || config.link; // Optional. Link can be a literal string or an i18n key reference.
-	var align = config.align || "center";        // Optional. "left", "center" or "right". Defaults to "center".
+  bus.send("ui-html:create", {
+    div : "footnote-container",
+    parentDiv : layout.map.attr("id"),
+    css : "footnote"
+  });
 
-	var container = $("<div/>").addClass("footnote").appendTo(layout.map);
+  var textDiv;
+  if (link) {
+    bus.send("ui-button:create", {
+      div : "footnote-link",
+      text : text,
+      parentDiv : "footnote-container",
+      css : "footnote-link",
+      sendEventName : "ui-open-url",
+      sendEventMessage : {
+        url : link,
+        target : "_blank"
+      }
+    });
+    textDiv = "footnote-link";
+  } else {
+    bus.send("ui-set-content", {
+      div : "footnote-container",
+      html : text
+    });
+    textDiv = "footnote-container";
+  }
 
-	if (link) {
-		$("<a />", {
-			href: link,
-			text: text,
-			target: "_blank"
-		}).css('text-align', align).appendTo(container);	
-	} else {
-		container.html(text).css('text-align', align);
-	}
-
+  bus.send("ui-css", {
+    div : textDiv,
+    key : "text-align",
+    value : align
+  });
 });
