0d11a51545b3cae5e53d9d6a34e9ed599caa5a6c
Víctor González <victor.gonzalez@geomati.co>
2016-12-20 16:55:23 +0100

    Migrate layer-order to use ui plugin

--- layer-order/src/main/resources/geoladris/layer-order/modules/layer-order.js
+++ layer-order/src/main/resources/geoladris/layer-order/modules/layer-order.js
@@ -4,51 +4,36 @@
 
 'use strict';
 
-define(["module", "toolbar", "i18n", "jquery", "message-bus", "map", "jquery-ui"], function(module, toolbar, i18n, $, bus, map) {
-	
-	var dialog = null;
-	var divContent = null;
-	var layers = [];
-	var layersLoaded = false;
+define([ "layout", "module", "toolbar", "i18n", "jquery", "message-bus", "map", "jquery-ui", "ui/ui" ], function(layout, module, toolbar, i18n, $, bus, map) {
+	var dialogId = "layer-order-pane";
 
-	/**
-	 * This function is also in legend-panel.js. Maybe is a good idea refactor it and extract a class to
-	 * generate UI components. But today is not the moment to do this. Micho García <micho.garcia@geomati.co>
-	 */
-	var getDialog_ = function() {
-		if (dialog == null) {
-			dialog = $("<div/>");
-			dialog.attr("title", i18n["layer_order"]);
-			dialog.attr("id", "layer_order_pane");
-			divContent = $("<div/>");
-			divContent.appendTo(dialog);
-			divContent.attr("id", "layer_order_content");
-			dialog.dialog({
-				position : {
-					my : "left top",
-					at : "right bottom+15",
-					of : "#order-button"
-				},
-				closeOnEscape : false,
-				autoOpen : false,
-				height : 500,
-				minHeight : 400,
-				width : 325,
-				zIndex : 2000,
-				resizable : true,
-				close: onCloseDialog_
-			});
-			divContent.sortable({
-				cursor: "move"
-			});
-			divContent.on('sortstop', onChangeLayerPosition_);
-		}
+	// Create ui components
+	bus.send("ui-button:create", {
+		div : "order-button",
+		parentDiv : toolbar.attr("id"),
+		css : "blue_button toolbar_button",
+		text : i18n["layer_order"],
+		sendEventName : "ui-toggle",
+		sendEventMessage : dialogId
+	});
 
-		return dialog;
-	};
-	
-	var onChangeLayerPosition_ = function(evt, ui) {
-		var newLayersOrder = divContent.sortable('toArray');
+	bus.send("ui-dialog:create", {
+		div : dialogId,
+		parentDiv : layout.map.attr("id"),
+		title : i18n["layer_order"],
+		closeButton : true
+	});
+	bus.send("ui-html:create", {
+		div : dialogId + "-content",
+		parentDiv : dialogId
+	});
+
+	var content = $("#" + dialogId + "-content");
+	content.sortable({
+		cursor : "move"
+	});
+	content.on("sortstop", function(evt, ui) {
+		var newLayersOrder = content.sortable('toArray');
 		for (var i = 0; i < newLayersOrder.length; i++) {
 			var id = newLayersOrder[i];
 			var layer = map.getLayer(id);
@@ -57,50 +42,42 @@ define(["module", "toolbar", "i18n", "jquery", "message-bus", "map", "jquery-ui"
 				// TODO: propagate change to other modules (and persist it in layers.json).
 			}
 		}
-	}
-	
-	var onCloseDialog_ = function(evt) {
-		divContent.empty();
-	}
+	});
 
-	var btn = $("<a/>").attr("id", "order-button").addClass("blue_button toolbar_button").html(i18n["layer_order"]);
-	btn.appendTo(toolbar);
-	btn.click(function() {
-		layers = map.layers;
-		var dialog = getDialog_();
-		if (!dialog.dialog("isOpen")) {
-			getDialog_().dialog("open");
-			showLayersOnDialog_();
-		} else {
-			getDialog_().dialog("close");
+	// Link dialog visibility and toolbar button
+	bus.listen("ui-toggle", function(e, id) {
+		if (id == dialogId) {
+			bus.send("ui-button:order-button:toggle");
 		}
 	});
-	
-	var insertLayerOnControl_ = function(layer) {
-		var item = $('<div>').attr('id', layer.id).addClass('layer-order-item');
-		var label = $('<span>').addClass('layer-name-item').html(layer.name);
-		item.append(label).appendTo(divContent);
-	}
-	
-	var showLayersOnDialog_ = function() {
-		if (layersLoaded) {
-			for (var n in layers) {
-				var layer = layers[n];
-				insertLayerOnControl_(layer);
-			}
-		} else {
-			// TODO poner aquí un gif loading
+	bus.listen("ui-hide", function(e, id) {
+		if (id == dialogId) {
+			bus.send("ui-button:order-button:activate", false);
 		}
-	}
+	});
+	bus.listen("ui-show", function(e, id) {
+		if (id == dialogId) {
+			bus.send("ui-button:order-button:activate", true);
+		}
+	});
 
-	var loadLayers_ = function() {
-		layersLoaded = true;
-	}
-
+	// Update content according to layers
 	bus.listen("reset-layers", function() {
-		layers = [];
-		layersLoaded = false;
+		bus.send("ui-set-content", {
+			div : dialogId + "-content",
+			content : ""
+		});
 	});
 
-	bus.listen('layers-loaded', loadLayers_);
+	bus.listen("layers-loaded", function() {
+		for ( var n in map.layers) {
+			var layer = map.layers[n];
+			bus.send("ui-html:create", {
+				div : layer.id,
+				parentDiv : dialogId + "-content",
+				css : "layer-order-item",
+				html : layer.name
+			});
+		}
+	});
 });
