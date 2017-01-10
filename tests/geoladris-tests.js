e3b1e43b7a51d2218478fc4fd17b95ef4fc9fe88
Víctor González <victor.gonzalez@geomati.co>
2017-01-10 14:09:04 +0100

    Improve geoladris-tests.js

--- tests/geoladris-tests.js
+++ tests/geoladris-tests.js
@@ -1,42 +1,57 @@
 define("geoladris-tests", [ "Squire", "message-bus" ], function(Squire, bus) {
-	// project basedir relative to the directory where karma.conf.js is
-	// contained; useful for RequireJS paths below
+  var CONTEXT = "geoladris-test";
+
+  // project basedir relative to the directory where karma.conf.js is
+  // contained; useful for RequireJS paths below
   var root = "../../../../../../";
   var injector;
-  
-  return {
-    init : function (name, config, paths) {
-      if (!paths) {
-        paths = {};
-      }
-      paths["jquery"] = root + "target/unpacked-core/geoladris/core/jslib/jquery-2.1.0";
-      paths["message-bus"] = root + "target/unpacked-core/geoladris/core/modules/message-bus";
 
-      var c = {
-        context : "geoladris-test",
-        baseUrl : "/base/src/main/resources/geoladris/" + name + "/modules",
-        paths : paths
-      };
+  function init(pluginName, config, paths) {
+    paths = paths || {};
+    paths["jquery"] = root + "target/unpacked-core/geoladris/core/jslib/jquery-2.1.0";
+    paths["message-bus"] = root + "target/unpacked-core/geoladris/core/modules/message-bus";
 
-      c.config = config;
-      require.config(c);
+    var c = {
+      context : CONTEXT,
+      baseUrl : "/base/src/main/resources/geoladris/" + pluginName + "/modules",
+      paths : paths
+    };
 
-      if (injector != null) {
-        injector.clean();
-        injector.remove();
-      }
+    c.config = config || {};
+    require.config(c);
 
-      injector = new Squire(c.context);
-      injector.mock("message-bus", bus);
-
-      spyOn(bus, "send").and.callThrough();
-      spyOn(bus, "listen").and.callThrough();
-
-      return {
-        bus : bus,
-        injector : injector
-      }
+    if (injector != null) {
+      injector.clean();
+      injector.remove();
     }
-	}
+
+    injector = new Squire(CONTEXT);
+    injector.mock("message-bus", bus);
+
+    bus.stopListenAll();
+    spyOn(bus, "send").and.callThrough();
+    spyOn(bus, "listen").and.callThrough();
+
+    return {
+      bus : bus,
+      injector : injector
+    }
+  }
+
+  function replaceParent(id) {
+    var previous = document.getElementById(id);
+    if (previous) {
+      document.body.removeChild(previous);
+    }
+
+    var parent = document.createElement('div');
+    parent.setAttribute("id", id);
+    document.body.appendChild(parent);
+  }
+
+  return {
+    init : init,
+    replaceParent : replaceParent
+  }
 });
 
