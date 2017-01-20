09a11c59c65df167dbdbbe283c32fd77826683ce
Víctor González <victor.gonzalez@geomati.co>
2017-01-20 13:27:01 +0100

    Migrate auth to use ui plugin

--- auth/src/main/resources/geoladris/modules/auth.js
+++ auth/src/main/resources/geoladris/modules/auth.js
@@ -1,4 +1,4 @@
-define([ "jquery", "message-bus", "i18n", "auth-user", "module", "jquery-ui" ], function($, bus, i18n, authUser, module) {
+define([ "message-bus", "i18n", "./auth-user", "module", "ui/ui" ], function(bus, i18n, authUser, module, ui) {
 	var LOGOUT = "logout";
 	var LOGIN = "login";
 	var DIALOG_ID = "auth-dialog";
@@ -6,8 +6,7 @@ define([ "jquery", "message-bus", "i18n", "auth-user", "module", "jquery-ui" ], 
 	var config = module.config();
 	var buttonConf = config.button;
 
-	var dialog;
-	var formDiv;
+	var form;
 	var userInput;
 	var passInput;
 
@@ -16,14 +15,14 @@ define([ "jquery", "message-bus", "i18n", "auth-user", "module", "jquery-ui" ], 
 		console.log(textStatus);
 		console.log(jqXHR);
 		// Disable inputs so their values are not appended to the URL
-		userInput.attr("disabled", "disabled");
-		passInput.attr("disabled", "disabled");
+		userInput.setAttribute("disabled", "disabled");
+		passInput.setAttribute("disabled", "disabled");
 
 		// Set redirection to the form and submit
 		var url = window.location.href;
 		url = url.substring(0, url.lastIndexOf("/") + 1);
-		formDiv.attr("action", url);
-		formDiv.submit();
+		form.setAttribute("action", url);
+		form.submit();
 	}
 
 	function doLogin(user, password) {
@@ -48,13 +47,13 @@ define([ "jquery", "message-bus", "i18n", "auth-user", "module", "jquery-ui" ], 
 	}
 
 	function initLogged() {
-		// Create button
-		var button = $("<div/>").attr("id", buttonConf.div);
-		button.addClass("auth-login-logout-button");
-		button.text(i18n["auth.logout"]);
-		button.appendTo($("#" + buttonConf.parentDiv));
-		button.click(function() {
-			bus.send("ajax", {
+		ui.create("button", {
+			id : buttonConf.div,
+			parent : buttonConf.parentDiv,
+			css : "auth-login-logout-button toolbar_button blue_button",
+			text : i18n["auth.logout"] || "Logout",
+			sendEventName : "ajax",
+			sendEventMessage : {
 				type : 'GET',
 				url : LOGOUT,
 				success : function() {
@@ -63,77 +62,57 @@ define([ "jquery", "message-bus", "i18n", "auth-user", "module", "jquery-ui" ], 
 				complete : function() {
 					window.location.reload();
 				}
-			});
+			}
 		});
 
 		bus.send("login", authUser);
 	}
 
 	function initNotLogged() {
-		// Create button
-		var button = $("<div/>").attr("id", buttonConf.div);
-		button.addClass("auth-login-logout-button");
-		button.text(i18n["auth.login"]);
-		button.appendTo($("#" + buttonConf.parentDiv));
-
-		// Create dialog
-		dialog = $("<div/>").attr("id", DIALOG_ID);
-		dialog.addClass("auth-dialog");
-		dialog.appendTo("body");
-
-		formDiv = $("<form/>").attr("id", DIALOG_ID + "-form");
-		dialog.append(formDiv);
-
-		var userContainer = $("<div/>").appendTo(formDiv).addClass("auth-input-container");
-		var userLabel = $("<div>" + i18n["auth.user"] + "<div>");
-		userLabel.attr("id", DIALOG_ID + "-user-label");
-		userLabel.addClass("auth-label");
-		userContainer.append(userLabel);
-
-		userInput = $("<input/>").attr("id", DIALOG_ID + "-user-input");
-		userInput.addClass("auth-input");
-		userInput.attr("type", "text");
-		userContainer.append(userInput);
-
-		var passContainer = $("<div/>").appendTo(formDiv).addClass("auth-input-container");
-		var passLabel = $("<div>" + i18n["auth.pass"] + "<div>");
-		passLabel.attr("id", DIALOG_ID + "-pass-label");
-		passLabel.addClass("auth-label");
-		passContainer.append(passLabel);
-
-		passInput = $("<input/>").attr("id", DIALOG_ID + "-pass-input");
-		passInput.addClass("auth-input");
-		passInput.attr("type", "password");
-		passContainer.append(passInput);
-
-		var send = $("<div/>").attr("id", DIALOG_ID + "-submit-button");
-		send.addClass("auth-submit blue_button");
-		send.attr("type", "submit");
-		send.text(i18n["auth.submit"]);
-		formDiv.append(send);
-
-		send.click(function() {
-			doLogin(userInput.val(), passInput.val());
+		ui.create("button", {
+			id : buttonConf.div,
+			parent : buttonConf.parentDiv,
+			css : "auth-login-logout-button toolbar_button blue_button",
+			text : i18n["auth.login"]  || "Login",
+			sendEventName : "ui-show",
+			sendEventMessage : DIALOG_ID
 		});
 
-		dialog.dialog({
-			autoOpen : false,
-			closeOnEscape : false,
-			width : "auto",
-			zIndex : 2000,
-			resizable : false,
-			position : {
-				my : "left top",
-				at : "left-300 bottom+40",
-				of : button
-			},
-			title : i18n["auth.title"]
+		ui.create("dialog", {
+			id : DIALOG_ID,
+			parent : "center",
+			css : "auth-dialog",
+			title : i18n["auth.title"],
+			closeButton : true
 		});
 
-		// Link button and dialog
-		button.click(function() {
-			dialog.dialog("open");
+		form = ui.create("form", {
+			id : DIALOG_ID + "-form",
+			parent : DIALOG_ID
 		});
+
+		userInput = ui.create("input", {
+			id : DIALOG_ID + "-user",
+			parent : DIALOG_ID + "-form",
+			css : "auth-input",
+			label : i18n["auth.user"]
+		});
+
+		passInput = ui.create("input", {
+			id : DIALOG_ID + "-pass",
+			parent : DIALOG_ID + "-form",
+			css : "auth-input",
+			label : i18n["auth.pass"],
+			type : "password"
+		});
+
+		var send = ui.create("div", {
+			id : DIALOG_ID + "-submit-button",
+			parent : DIALOG_ID + "-form",
+			css : "auth-submit blue-button",
+			html : i18n["auth.submit"]
+		});
+		send.setAttribute("type", "submit");
 	}
 
 	if (authUser) {
