7bdbd2886ea033aae5ce81126a012ffd1c8bfbe1
Víctor González <victor.gonzalez@geomati.co>
2017-01-21 13:44:58 +0100

    Fix layers-editor tests

--- layers-editor/src/test/resources/js/tests/save-buttonTest.js
+++ layers-editor/src/test/resources/js/tests/save-buttonTest.js
@@ -12,16 +12,19 @@ define([ "geoladris-tests" ], function(tests) {
 		};
 		var injector;
 		var bus;
+		var ui;
 
 		beforeEach(function() {
 			var initialization = tests.init("layers-editor", {});
 			injector = initialization.injector;
 			bus = initialization.bus;
 
+			ui = jasmine.createSpyObj("ui", [ "create" ]);
+
 			toolbar = $("<div/>").attr("id", "toolbar");
 			$("body").append(toolbar);
 			injector.mock("toolbar", toolbar);
-
+			injector.mock("ui/ui", ui);
 			injector.mock("./layers-api", api);
 			injector.mock("i18n", i18n);
 		});
@@ -32,28 +35,13 @@ define([ "geoladris-tests" ], function(tests) {
 
 		it("creates a button", function(done) {
 			injector.require([ "save-button" ], function(saveButton) {
-				var button = $("#save-layers-button");
-				expect(button.length).toBe(1);
-				expect(button.parent().attr("id")).toBe("toolbar");
-				expect(button.hasClass("blue_button")).toBe(true);
-				expect(button.hasClass("toolbar_button")).toBe(true);
-				expect(button.html()).toBe(i18n["layers-editor.save_layers"]);
-
-				done();
-			});
-		});
-
-		it("sends ajax on click", function(done) {
-			injector.require([ "save-button" ], function(saveButton) {
-				var button = $("#save-layers-button");
-				button.click();
-				expect(bus.send).toHaveBeenCalledWith("ajax", jasmine.objectContaining({
-					type : 'PUT',
-					url : 'layers.json',
-					contentType : "application/json; charset=utf-8",
-					errorMsg : i18n["layers-editor.error_saving"]
+				expect(ui.create).toHaveBeenCalledWith("button", jasmine.objectContaining({
+					id : "save-layers-button",
+					parent : "toolbar",
+					css : "blue_button toolbar_button",
+					html : i18n["layers-editor.save_layers"],
+					sendEventName : "ajax"
 				}));
-
 				done();
 			});
 		});
