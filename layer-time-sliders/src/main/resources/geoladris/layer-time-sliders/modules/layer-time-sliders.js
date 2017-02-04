8e885623a5d543b29a0a112281aa846959fea6c6
Víctor González <victor.gonzalez@geomati.co>
2017-02-04 14:24:23 +0100

    Migrate layer-time-sliders to use ui

--- layer-time-sliders/src/main/resources/geoladris/layer-time-sliders/modules/layer-time-sliders.js
+++ layer-time-sliders/src/main/resources/geoladris/layer-time-sliders/modules/layer-time-sliders.js
@@ -1,105 +1,91 @@
-define([ "jquery", "message-bus", "layout", "map", "layer-list-selector", "moment", "jquery-ui" ], function($, bus, layout, map, layerListSelector, moment) {
+define([ "jquery", "message-bus", "layout", "map", "layer-list-selector", "moment", "ui/ui" ], function($, bus, layout, map, layerListSelector, moment, ui) {
 	var aTimestampsLayers = {};
-	var divTimeSliders = $("<div/>").attr("id", "layerTimeSliders").addClass("layer_container_panel");
-	layerListSelector.registerLayerPanel("layer_slider_selector", 30, "Temporal", divTimeSliders);
 
-	var formatDate = function(date, dateFormat) {
-		var format;
-		if (dateFormat) {
-			format = dateFormat;
-		} else {
-			format = "YYYY";
-		}
-		return moment(date).format(format);
+	var sliders = {};
+	var labels = {};
+	var container = ui.create("div", {
+		id : "layerTimeSliders",
+		css : "layer_container_panel"
+	})
+	layerListSelector.registerLayerPanel("layer_slider_selector", 30, "Temporal", $(container));
+
+	function setLabel(id, date, dateFormat) {
+		var label = labels[id] + " (" + moment(date).format(dateFormat || "YYYY") + ")";
+		bus.send("ui-input:layer_time_slider_" + id + ":set-label", label);
 	}
 
 	bus.listen("reset-layers", function() {
-		$(".layer-time-slider").slider("destroy");
-		$(".layer-time-slider-container").remove();
+		container.innerHTML = "";
 		aTimestampsLayers = {};
+		sliders = {};
+		labels = {};
 	});
 
 	bus.listen("add-layer", function(event, layerInfo) {
-		var timestamps = [];
 		var layerTimestamps = layerInfo.timestamps;
-		if (layerTimestamps != null) {
-			for (var i = 0; i < layerTimestamps.length; i++) {
-				var d = new Date();
-				d.setISO8601(layerTimestamps[i]);
-				timestamps.push(d);
-			}
+		if (!layerTimestamps || !layerTimestamps.length) {
+			return;
 		}
 
-		if (timestamps.length > 0) {
-			timestamps.sort(function(a, b) {
-				return a - b;
+		var timestamps = [];
+		for (var i = 0; i < layerTimestamps.length; i++) {
+			var d = new Date();
+			d.setISO8601(layerTimestamps[i]);
+			timestamps.push(d.getTime());
+		}
+		var lastTimestamp = timestamps[timestamps.length - 1];
+
+		var slider = ui.create("slider", {
+			id : "layer_time_slider_" + layerInfo.id,
+			parent : container,
+			css : "layer-time.slider",
+			values : timestamps,
+			value : lastTimestamp
+		});
+		sliders[layerInfo.id] = slider;
+		labels[layerInfo.id] = layerInfo.label;
+
+		slider.addEventListener("change", function(event) {
+			var date = new Date(event.detail.value);
+			layerInfo.mapLayers.forEach(function(mapLayer) {
+				var layer = map.getLayer(mapLayer.id);
+				layer.mergeNewParams({
+					'time' : date.toISO8601String()
+				});
+				bus.send("layer-time-slider.selection", [ layerInfo.id, date ]);
 			});
+		});
 
-			var divTimeSliderContainer = $("<div id='layer_time_slider_container_" + layerInfo.id + "'/>").addClass("layer-time-slider-container").appendTo(divTimeSliders);
-			var divTimeSliderTitle = $("<div/>").html(layerInfo.label).addClass("layer-time-slider-title").appendTo(divTimeSliderContainer);
-			var divTimeSliderLabel = $("<span id='layer_time_slider_label_" + layerInfo.id + "'/>").appendTo(divTimeSliderContainer);
-			var divTimeSlider = $("<div id='layer_time_slider_" + layerInfo.id + "' class='layers_time_slider' />").appendTo(divTimeSliderContainer);
-			divTimeSlider.addClass("layer-time-slider");
+		slider.addEventListener("slide", function(event) {
+			setLabel(layerInfo.id, new Date(event.detail.value), layerInfo["date-format"]);
+		});
 
-			divTimeSlider.slider({
-				change : function(event, ui) {
-					if (event.originalEvent) {
-						var date = timestamps[ui.value];
-						$.each(layerInfo.mapLayers, function(index, mapLayer) {
-							var layer = map.getLayer(mapLayer.id);
-							layer.mergeNewParams({
-								'time' : date.toISO8601String()
-							});
-							bus.send("layer-time-slider.selection", [ layerInfo.id, date ]);
-						});
-					} else { // Programatic change
-						// alert('programatic');
-					}
-					;
+		setLabel(layerInfo.id, new Date(lastTimestamp), layerInfo["date-format"]);
 
-				},
-				slide : function(event, ui) {
-					var date = timestamps[ui.value];
-					divTimeSliderLabel.text(formatDate(date, layerInfo["date-format"]));
-				},
-				max : timestamps.length - 1,
-				value : timestamps.length - 1
-			});
-
-			divTimeSliderLabel.text(formatDate(timestamps[timestamps.length - 1], layerInfo["date-format"]));
-
-			var timestampInfo = {
-				"timestamps" : timestamps
-			};
-			if (layerInfo["date-format"]) {
-				timestampInfo["date-format"] = layerInfo["date-format"];
-			}
-
-			aTimestampsLayers[layerInfo.id] = timestampInfo;
+		var timestampInfo = {
+			"timestamps" : timestamps
+		};
+		if (layerInfo["date-format"]) {
+			timestampInfo["date-format"] = layerInfo["date-format"];
 		}
 
+		aTimestampsLayers[layerInfo.id] = timestampInfo;
 	});
 
 	bus.listen("layer-timestamp-selected", function(e, layerId, d) {
 		var timestampInfo = aTimestampsLayers[layerId];
-		var position_i = -1;
-		$.each(timestampInfo["timestamps"], function(position, date_value) {
-			if (date_value.valueOf() == d.valueOf()) {
-				position_i = position;
-				$('#layer_time_slider_' + layerId).slider('value', position_i);
-
-				var strDate = formatDate(d, timestampInfo["date-format"]);
-				$('#layer_time_slider_label_' + layerId).text(strDate);
+		timestampInfo["timestamps"].forEach(function(timestamp) {
+			if (timestamp == d.getTime()) {
+				bus.send("ui-slider:layer_time_slider_" + layerId + ":set-value", timestamp);
+				setLabel(layerId, d, timestampInfo["date-format"]);
 			}
 		});
 	});
 
 	bus.listen("layer-visibility", function(e, layerId, visible) {
-		var container = $("#layer_time_slider_container_" + layerId);
-		if (visible) {
-			container.show();
-		} else {
-			container.hide();
+		var slider = sliders[layerId];
+		if (slider) {
+			slider.style.display = visible ? "" : "none";
 		}
 	});
 });
