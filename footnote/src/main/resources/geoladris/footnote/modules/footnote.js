d6a8ae715a71b72df93e7b9cade53ee3ef0044ae
Fernando González Cortés <fernando.gonzalez@geomati.co>
2017-02-22 08:52:46 +0100

    multiple notes in footnote

--- footnote/src/main/resources/geoladris/footnote/modules/footnote.js
+++ footnote/src/main/resources/geoladris/footnote/modules/footnote.js
@@ -1,36 +1,60 @@
-define([ "message-bus", "module", "layout", "i18n", "jquery", "ui/ui" ], function(bus, module, layout, i18n, $, ui) {
+define([ "message-bus", "module", "i18n", "jquery", "ui/ui" ], function(bus, module, i18n, $, ui) {
   var config = module.config();
-  // Required. Text can be a literal string or an i18n key reference.
-  var text = i18n[config.text] || config.text;
-  // Optional. Link can be a literal string or an i18n key reference.
-  var link = i18n[config.link] || config.link;
+  
+  var links = [];
+  if (config.hasOwnProperty("notes")) {
+	  for (var i = 0; i < config.notes.length; i++) {
+		note = config.notes[i];
+		links.push({
+			text : i18n[note.text] || note.text,
+			link : i18n[note.link] || note.link
+		});
+	  }
+  } else {
+	  links.push({
+		  text : i18n[config.text] || config.text,
+		  link : i18n[config.link] || config.link
+	  });
+  }
+  
   // Optional. "left", "center" or "right". Defaults to "center".
   var align = config.align || "center";
 
-  var container = ui.create("div", {
-    id : "footnote-container",
-    parent : layout.map.attr("id"),
-    css : "footnote"
+  var container;
+  
+  bus.listen("modules-initialized", function(e, message) {
+	  var parent = null;
+	  if (config.hasOwnProperty("htmlId")) {
+		  parent = config.htmlId;
+		  if (parent == null) { 
+			  parent = document.body;
+		  }
+	  } else {
+		  // backwards compatibility
+		  parent = "map"
+	  }
+	  container = ui.create("div", {
+		  id : "footnote-container",
+		  parent : parent,
+		  css : "footnote"
+	  });
+	  for (var i = 0; i < links.length; i++) {
+		  var btn = ui.create("a", {
+			  id : "footnote-link-" + i,
+			  parent : "footnote-container",
+			  html : links[i].text,
+			  css : "footnote-link"
+		  });
+		  btn.href = links[i].link;
+		  btn.target = "_blank";
+		  btn.style["text-align"] = align;
+		  if (i < links.length - 1) {
+			  ui.create("span", {
+				  parent : "footnote-container",
+				  html : " - ",
+				  css : "footnote-link"				  
+			  });
+		  }
+	  }
   });
-
-  var textDiv;
-  if (link) {
-    ui.create("button", {
-      id : "footnote-link",
-      parent : "footnote-container",
-      text : text,
-      css : "footnote-link",
-      clickEventName : "ui-open-url",
-      clickEventMessage : {
-        url : link,
-        target : "_blank"
-      }
-    });
-    textDiv = "footnote-link";
-  } else {
-    container.innerHTML = text;
-    textDiv = "footnote-container";
-  }
-
-  document.getElementById(textDiv).style["text-align"] = align;
 });
